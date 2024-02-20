f28abe5694abc123ca5ac0ffa2eaebe767e3f5cc
smahesh77 <smahesh7736@gmail.com>
2024-02-20 23:34:55 +0530

    initial

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+      molestiae quod facere officiis aut corrupti omnis dolor magnam beatae
+      minus sapiente nisi aperiam, id cum. Fuga mollitia laborum nulla ipsa.
+      <Button variant="ghost">try me</Button>
+    </>
+  );
+}
+
+export default App;
