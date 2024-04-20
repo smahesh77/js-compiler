c294a1da5e759ce96de72bed736832e5dcfb593a
S Mahesh <98635199+smahesh77@users.noreply.github.com>
2024-04-20 14:12:29 +0530

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="w-full h-[calc(100dvh-60px)] text-white flex items-center justify-center flex-col gap-3">
-      <h1 className="text-6xl font-bold text-center">jCOM</h1>
+      <h1 className="text-6xl font-bold text-center">jCompiler</h1>
       <p className="text-gray-500 text-center">Compile HTML,css and JS code on the go!!!</p>
     </div>
   );
