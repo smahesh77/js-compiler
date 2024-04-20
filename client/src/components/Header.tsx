26b755844e5b7fa4275b9ea06e7977a96b381796
S Mahesh <98635199+smahesh77@users.noreply.github.com>
2024-04-20 14:14:24 +0530

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <nav className="flex w-full h-[60px] bg-gray-900 text-white p-3 justify-between items-center">
       <Link to="/">
-        <h2 className=" font- select-none">jCOM</h2>
+        <h2 className=" font- select-none">jCompiler</h2>
       </Link>
       <ul className="flex gap-2">
         <li>
